a572e78fa1872cb4e287186128bd50f679867a05
rafael <rafaelaparecido.oliveirasilva@gmail.com>
2022-12-08 18:24:21 -0300

    Adicionando início do jogo e mudança de turnos

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
 const cellElements = document.querySelectorAll('[data-cell]');
-let isCircleTurn = false;
+const board = document.querySelector('[data-board]');
+let isCircleTurn;
+
+const startGame = () => {
+  for(const cell of cellElements){
+    cell.addEventListener('click', handleClick, {once: true});
+  }
+
+  isCircleTurn = false;
+
+  board.classList.add('x');
+
+}
 
 const placeMark = (cell, classToAdd) => {
   cell.classList.add(classToAdd);
+}
+
+const swipTurn = () => {
+  /* isCircleTurn recebe o inverso que ele tinha */
+  isCircleTurn = !isCircleTurn;
+
+  /* Pra não ser adicionado várias vezes as classes x ou o, vamos removê-las na mudança de turno */
+  board.classList.remove('x');
+  board.classList.remove('circle');
+
+  if(isCircleTurn){
+    board.classList.add('circle');
+  } else {
+    board.classList.add('x');
+  }
 }
 
 const handleClick = (e) => {
@@ -17,9 +44,8 @@ const handleClick = (e) => {
   // Verificar empate
 
   // Trocar jogador
+  swipTurn();
 }
 
-for(const cell of cellElements){
-    cell.addEventListener('click', handleClick, {once: true});
-}
+startGame();
 
